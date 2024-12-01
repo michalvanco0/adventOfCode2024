205f33896c255fa38c03e114ce06126fa8cbf4b2
Michal Vančo <111017455+michalvanco0@users.noreply.github.com>
2024-12-01 06:43:11 +0100

    Add files via upload

---
+++ advent1.kt
@@ -0,0 +1,32 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val lst1 = mutableListOf<Int>()
+    val lst2 = mutableListOf<Int>()
+
+    File("C:\\Users\\User\\Java\\input.txt").forEachLine { line ->
+        val parts = line.trim().split("\\s+".toRegex())
+        if (parts.size == 2) {
+            val num1 = parts[0].toInt()
+            val num2 = parts[1].toInt()
+            lst1.add(num1)
+            lst2.add(num2)
+        }
+    }
+    lst1.sort()
+    lst2.sort()
+    var count = 0
+    for (i in lst1.indices) {
+        count += abs(lst1[i] - lst2[i])
+    }
+//    println(count)
+
+//    2
+    var score = 0
+    val appearances = lst2.groupingBy { it }.eachCount()
+    for (i in lst1.indices) {
+        score += lst1[i] * (appearances[lst1[i]] ?: 0)
+    }
+    println(score)
+}
